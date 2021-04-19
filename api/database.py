e386706bbfad4b0f576e20cccfad4fe5bcff047d
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 <grisov.github@mailnull.com>
2021-04-19 17:15:28 +0300

    Added methods for updating the record and searching in the database

--- api/database.py
+++ api/database.py
@@ -26,7 +26,7 @@ class Database(object):
                     amount INTEGER NOT NULL
                 )"""
             )
-        except sqlite3.Error as e:
+        except sqlite3.Error:
             pass
 
     def get(self, id: int) -> Optional[Course]:
@@ -43,7 +43,7 @@ class Database(object):
             self._cur.execute(query, (id,))
             record = self._cur.fetchone()
         except sqlite3.Error as e:
-            pass
+            print(e)
         if record:
             return Course(
                 name=record[1],
@@ -69,6 +69,7 @@ class Database(object):
             course.id = self._cur.lastrowid or -1
             self._conn.commit()
         except sqlite3.Error as e:
+            print(e)
             return None
         return course
 
@@ -87,6 +88,7 @@ class Database(object):
             self._cur.execute(query, (id,))
             self._conn.commit()
         except sqlite3.Error as e:
+            print(e)
             return None
         return course
 
@@ -101,8 +103,19 @@ class Database(object):
         """
         if id is None or id <= 0 or course is None:
             return None
+        if self.get(id) is None:
+            return self.add(course)
+        query = "UPDATE courses SET name=?, start=?, end=?, amount=? WHERE id=?"
+        try:
+            self._cur.execute(query, (course.name, course.start.isoformat(), course.end.isoformat(), course.amount, id))
+            self._conn.commit()
+        except sqlite3.Error as e:
+            print(e)
+            return None
+        course.id = id
+        return course
 
-    def search(self, data: Optional[SearchData]) -> List[Course]:
+    def search(self, data: Optional[SearchData]=None) -> List[Course]:
         """Search for courses that match the query data.
         :param data: the search query data
         :type data: Optional[SearchData]
@@ -110,7 +123,27 @@ class Database(object):
         :rtype: List[Course]
         """
         if data is None:
-            return []
+            data = SearchData()
+        query = """SELECT * FROM courses
+            WHERE instr(name, ?) AND start >= ? AND end <= ?
+            ORDER BY start ASC, name ASC"""
+        try:
+            self._cur.execute(query, (
+                    data.name or "",
+                    data.start or "1970-01-01",
+                    data.end or "9999-12-31"
+                )
+            )
+            records = self._cur.fetchall()
+        except sqlite3.Error as e:
+            print(e)
+        return [Course(
+                name=rec[1],
+                start=rec[2],
+                end=rec[3],
+                amount=rec[4],
+                id=rec[0]
+            ) for rec in records]
 
     def close(self) -> None:
             self._cur.close()
