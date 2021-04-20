d353a6b6115ee80f20c992d8b9c25f70aa24f4c7
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 <grisov.github@mailnull.com>
2021-04-20 08:21:48 +0300

    Added the controller that deletes the course from the database

--- api/controllers/general.py
+++ api/controllers/general.py
@@ -1,13 +1,12 @@
 import connexion
-from typing import Union
+from typing import Union, Optional
 from api.models.course import Course
 from api.models.error import Error
 from api.database import Database
 
 
 def get_details(cid: int) -> Union[Course, Error]:
-    """Detailed information about the training course.
-    Complete information about the training course with the specified ID.
+    """Detailed information about the training course with the specified ID.
     :param cid: The ID of the Training course in the database
     :type cid: int
     :return: the detailed information about the training course
@@ -17,31 +16,41 @@ def get_details(cid: int) -> Union[Course, Error]:
     if course is not None:
         return course
     return Error(
-            status=214,
-            title="No Data",
+            status=204,
+            title="No Content",
             detail=f"There is no record with index {cid} in the database",
             type="about:blank"
-        ), 214
+        ), 204
 
 
-def remove(id: int):
-    """Delete a course with the specified ID.
-    Delete a training course from the database.
-    :param id: The ID of the Training course in the database
-    :type id: int
-    :rtype: Course
+def remove(cid: int) -> Union[Course, Error]:
+    """Delete a course with the specified ID from DB.
+    :param cid: The ID of the Training course in the database
+    :type cid: int
+    :return: the detailed information about the deleted course
+    :rtype: Union[Course, Error]
     """
-    return 'do some magic!'
+    db = Database()
+    course = db.remove(cid)
+    db.close()
+    if course is not None:
+        return course
+    return Error(
+            status=204,
+            title="No Content",
+            detail=f"There is no record with index {cid} in the database",
+            type="about:blank"
+        ), 204
 
 
-def update(id: int, course: Course=None):
-    """Update a course with the specified ID.
-    Change the properties of the training course in the database.
-    :param id: The ID of the Training course in the database
-    :type id: int
-    :param course: A set of data describing the training course
-    :type course: dict | bytes
-    :rtype: Course
+def update(cid: int, course: Optional[Course]=None) -> Union[Course, Error]:
+    """Update the properties of the training course in the DB.
+    :param cid: The ID of the Training course in the database
+    :type cid: int
+    :param course: new training course data for replacement
+    :type course: Optional[Course]
+    :return: the updated information about the course
+    :rtype: Union[Course, Error]
     """
     if connexion.request.is_json:
         course = Course.from_dict(connexion.request.get_json())
