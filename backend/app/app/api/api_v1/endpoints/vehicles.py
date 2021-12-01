ea6cabf763bf6fe92d7fea1be38765532eeb38fe
grisov <grisov@gmail.com>
2021-12-01 22:55:03 +0200

    Added endpoint to get all vehicles from the database

--- backend/app/app/api/api_v1/endpoints/vehicles.py
+++ backend/app/app/api/api_v1/endpoints/vehicles.py
@@ -1,7 +1,28 @@
 from typing import Any, List
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from app import crud, models, schemas
 from app.api import deps
 
 router = APIRouter()
+
+
+@router.get("/vehicle/", response_model=List[schemas.VehicleDatabase])
+def get_all_vehicles(
+    *,
+    db: Session = Depends(deps.get_db)
+) -> Any:
+    try:
+        vehicles = crud.vehicle.get_multi(db, limit=crud.vehicle.count(db))
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Unable to connect to the database: %s" % str(e)
+        )
+    if not vehicles:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There are no vehicles in the database"
+        )
+    return [schemas.VehicleDatabase(**jsonable_encoder(vehicle)) for vehicle in vehicles]
