3c437bfb3f3b4c24179a1b55c0f0e97dba16b106
grisov <grisov@gmail.com>
2021-12-01 22:47:04 +0200

    Added endpoint to get all drivers from the database

--- backend/app/app/api/api_v1/endpoints/drivers.py
+++ backend/app/app/api/api_v1/endpoints/drivers.py
@@ -1,7 +1,28 @@
 from typing import Any, List
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from app import crud, models, schemas
 from app.api import deps
 
 router = APIRouter()
+
+
+@router.get("/driver/", response_model=List[schemas.DriverDatabase])
+def get_all_drivers(
+    *,
+    db: Session = Depends(deps.get_db)
+) -> Any:
+    try:
+        drivers = crud.driver.get_multi(db, limit=crud.driver.count(db))
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Unable to connect to the database: %s" % str(e)
+        )
+    if not drivers:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There are no drivers in the database"
+        )
+    return [schemas.DriverDatabase(**jsonable_encoder(driver)) for driver in drivers]
