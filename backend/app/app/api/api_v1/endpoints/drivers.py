73356f03dbf9b36d35f0194c58e2cf73b5ca24c1
Oleksandr Gryshchenko <grisov@gmail.com>
2021-12-03 01:04:17 +0200

    Added text comments to /drivers/* endpoints

--- backend/app/app/api/api_v1/endpoints/drivers.py
+++ backend/app/app/api/api_v1/endpoints/drivers.py
@@ -1,7 +1,7 @@
 from typing import Any, List, Optional
 from fastapi import APIRouter, Depends, HTTPException, Query, status
 from fastapi.encoders import jsonable_encoder
-from pydantic import ValidationError
+from pydantic import PositiveInt, ValidationError
 from sqlalchemy.orm import Session
 from app import crud, models, schemas
 from app.api import deps
@@ -12,14 +12,18 @@ router = APIRouter()
 @router.get(
     path="/driver/",
     response_model=List[schemas.DriverDatabase],
-    summary="",
-    description="")
+    summary="Get the list of drivers from the database",
+    description="The list of drivers can be filtered by registration date")
 def get_drivers(
     created_at__gte: Optional[str] = Query(default=None, regex="^\d{1,2}-\d{1,2}-\d{4}$", title="Start date"),
     created_at__lte: Optional[str] = Query(default=None, regex="^\d{1,2}-\d{1,2}-\d{4}$", title="End date"),
     *,
     db: Session = Depends(deps.get_db)
 ) -> Any:
+    """Get a list of drivers, filtered by date of registration if nesesery.
+    :param created_at__gte: registration starting from this date (inclusive)
+    :param created_at__lte: registration before this date
+    """
     try:
         created_at = schemas.CreatedAt(gte=created_at__gte, lte=created_at__lte)
     except (ValueError, ValidationError) as ve:
@@ -42,12 +46,19 @@ def get_drivers(
     return [schemas.DriverDatabase(**jsonable_encoder(driver)) for driver in drivers]
 
 
-@router.get("/driver/{driver_id}/", response_model=schemas.DriverDatabase)
+@router.get(
+    path="/driver/{driver_id}/",
+    response_model=schemas.DriverDatabase,
+    summary="Detailed information about the driver",
+    description="Get complete information about the driver by his ID from the database")
 def get_driver_by_id(
-    driver_id: int,
+    driver_id: PositiveInt,
     *,
     db: Session = Depends(deps.get_db)
 ) -> Any:
+    """Get detailed information about the driver.
+    :param driver_id: driver ID in the database
+    """
     try:
         driver = crud.driver.get(db, id=driver_id)
     except Exception as e:
@@ -63,12 +74,19 @@ def get_driver_by_id(
     return schemas.DriverDatabase(**jsonable_encoder(driver))
 
 
-@router.post("/driver/", response_model=schemas.DriverDatabase)
+@router.post(
+    path="/driver/",
+    response_model=schemas.DriverDatabase,
+    summary="Add new driver",
+    description="Create a new driver in the database")
 def add_driver(
     driver_in: schemas.DriverCreate,
     *,
     db: Session = Depends(deps.get_db)
 ) -> Any:
+    """Add new driver to the database.
+    :param driver_in: detailed information about the driver
+    """
     try:
         driver = crud.driver.create(db, obj_in=driver_in)
     except Exception as e:
@@ -79,13 +97,21 @@ def add_driver(
     return schemas.DriverDatabase(**jsonable_encoder(driver))
 
 
-@router.patch("/driver/{driver_id}/", response_model=schemas.DriverDatabase)
+@router.patch(
+    path="/driver/{driver_id}/",
+    response_model=schemas.DriverDatabase,
+    summary="Update driver information",
+    description="Update driver details with specified ID")
 def update_driver(
-    driver_id: int,
+    driver_id: PositiveInt,
     driver_in: schemas.DriverUpdate,
     *,
     db: Session = Depends(deps.get_db)
 ) -> Any:
+    """Update the driver details.
+    :param driver_id: driver ID in the database
+    :param driver_in: new details about the driver
+    """
     try:
         driver = crud.driver.get(db, id=driver_id)
     except Exception as e:
@@ -108,12 +134,19 @@ def update_driver(
     return schemas.DriverDatabase(**jsonable_encoder(updated_driver))
 
 
-@router.delete("/driver/{driver_id}/", response_model=schemas.DriverDatabase)
+@router.delete(
+    path="/driver/{driver_id}/",
+    response_model=schemas.DriverDatabase,
+    summary="Delete the driver",
+    description="Remove the driver with the specified ID from the database")
 def delete_driver(
     driver_id: int,
     *,
     db: Session = Depends(deps.get_db)
 ) -> Any:
+    """Delete the driver from the database.
+    :param driver_id: driver ID in the database
+    """
     try:
         driver = crud.driver.remove(db, id=driver_id)
     except Exception as e:
