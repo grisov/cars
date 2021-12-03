7117f54a41aa4624f25c76b2df6852448d403511
Oleksandr Gryshchenko <grisov@gmail.com>
2021-12-03 02:40:57 +0200

    Added text comments to vehicles/* endpoints

--- backend/app/app/api/api_v1/endpoints/vehicles.py
+++ backend/app/app/api/api_v1/endpoints/vehicles.py
@@ -1,6 +1,7 @@
 from typing import Any, List, Literal, Optional
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.encoders import jsonable_encoder
+from pydantic import PositiveInt
 from sqlalchemy.orm import Session
 from app import crud, models, schemas
 from app.api import deps
@@ -36,12 +37,19 @@ def get_vehicles(
     return [schemas.VehicleDatabase(**jsonable_encoder(vehicle)) for vehicle in vehicles]
 
 
-@router.get("/vehicle/{vehicle_id}/", response_model=schemas.VehicleDatabase)
+@router.get(
+    path="/vehicle/{vehicle_id}/",
+    response_model=schemas.VehicleDatabase,
+    summary="Vehicle information",
+    description="Get detailed information about a particular vehicle by its ID")
 def get_vehicle_by_id(
-    vehicle_id: int,
+    vehicle_id: PositiveInt,
     *,
     db: Session = Depends(deps.get_db)
 ) -> Any:
+    """Get detailed information about the vehicle.
+    :param vehicle_id: vehicle ID in the database
+    """
     try:
         vehicle = crud.vehicle.get(db, id=vehicle_id)
     except Exception as e:
@@ -57,12 +65,19 @@ def get_vehicle_by_id(
     return schemas.VehicleDatabase(**jsonable_encoder(vehicle))
 
 
-@router.post("/vehicle/", response_model=schemas.VehicleDatabase)
+@router.post(
+    path="/vehicle/",
+    response_model=schemas.VehicleDatabase,
+    summary="Add new vehicle",
+    description="Create a new vehicle in the database")
 def add_vehicle(
     vehicle_in: schemas.VehicleCreate,
     *,
     db: Session = Depends(deps.get_db)
 ) -> Any:
+    """Create a new vehicle in the database.
+    :param vehicle_in: information about the vehicle to be added to the database
+    """
     try:
         vehicle = crud.vehicle.create(db, obj_in=vehicle_in)
     except Exception as e:
@@ -73,13 +88,21 @@ def add_vehicle(
     return schemas.VehicleDatabase(**jsonable_encoder(vehicle))
 
 
-@router.patch("/vehicle/{vehicle_id}/", response_model=schemas.VehicleDatabase)
+@router.patch(
+    path="/vehicle/{vehicle_id}/",
+    response_model=schemas.VehicleDatabase,
+    summary="Update vehicle information",
+    description="Update vehicle details in the database")
 def update_vehicle(
-    vehicle_id: int,
+    vehicle_id: PositiveInt,
     vehicle_in: schemas.VehicleUpdate,
     *,
     db: Session = Depends(deps.get_db)
 ) -> Any:
+    """Update vehicle details in the database.
+    :param vehicle_id: vehicle ID in the database
+    :param vehicle_in: new vehicle information to be updated in the database
+    """
     try:
         vehicle = crud.vehicle.get(db, id=vehicle_id)
     except Exception as e:
@@ -102,12 +125,19 @@ def update_vehicle(
     return schemas.VehicleDatabase(**jsonable_encoder(updated_vehicle))
 
 
-@router.delete("/vehicle/{vehicle_id}/", response_model=schemas.VehicleDatabase)
+@router.delete(
+    path="/vehicle/{vehicle_id}/",
+    response_model=schemas.VehicleDatabase,
+    summary="Delete the vehicle",
+    description="Remove the specified vehicle from the database")
 def delete_vehicle(
-    vehicle_id: int,
+    vehicle_id: PositiveInt,
     *,
     db: Session = Depends(deps.get_db)
 ) -> Any:
+    """Delete the specified vehicle from the database.
+    :param vehicle_id: vehicle ID in the database
+    """
     try:
         vehicle = crud.vehicle.remove(db, id=vehicle_id)
     except Exception as e:
